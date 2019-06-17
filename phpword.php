a8560a388b9c44b7cc3435ed5e26d96f7bcbada0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-06-17 14:38:35 +0100

    setting error reporting back to default

--- phpword.php
+++ phpword.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ALL); ini_set('display_errors', 1);
+error_reporting(0);
 $file_dir = "/sites/default/files/filerepository/";
 
 //DONT CHANGE ANYTHING BELOW HERE--------------------------------------------------------------------------------------------------------------------
