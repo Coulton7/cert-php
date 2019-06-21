79cee00343309843f0fe43ff81157091c5492b41
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-06-21 14:44:39 +0100

    Back to the file need to figure howto find excel and print files

--- phpword.php
+++ phpword.php
@@ -27,9 +27,9 @@ exit();
 
 clearstatcache();
 
-if (realpath($userDoc.".doc") || realpath($userDoc.".xls")) {
+if (file($userDoc.".doc") || file($userDoc.".xls")) {
 
-if(realpath($userDoc.".doc")){
+if(file($userDoc.".doc")){
 $text = parseWord($userDoc.".doc");//Method 2
 //$html = nl2br(htmlspecialchars($text));
 //$html = preg_replace('/\s\s+/', ' ', $html);
