ed6a550023173f9d9f79e3d1ff5676115aa2367d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-06-21 10:40:39 +0100

    smallcorrection to stop error 500

--- phpword.php
+++ phpword.php
@@ -95,7 +95,7 @@ else{
 echo '   <div class="success">
       <div class="notice"><img src="/sites/default/files/cert-images/error.png" width="64" height="64" align="absmiddle" /><strong>Sorry</strong> We couldn\'t find that Certificate</div>
     </div>
-    <p><div class="cert-footer">Please check the Certificate number and try again, or please <a href="https://www.avtreliability.com/contact-us">contact us</a> for a manual validation.</div><p>'.$userDoc.".doc"'</p>
+    <p><div class="cert-footer">Please check the Certificate number and try again, or please <a href="https://www.avtreliability.com/contact-us">contact us</a> for a manual validation.</div><p>'.$userDoc.'.doc</p>
    <p>&nbsp;</p>';
 
 }
