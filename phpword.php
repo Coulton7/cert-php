2a0f6b35665be9ff4f68f81909983b99a1a9de8a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-06-24 14:41:07 +0100

    absolute path being used now and version number again

--- phpword.php
+++ phpword.php
@@ -1,7 +1,7 @@
 <?php
 
 error_reporting(0);
-$file_dir = "../sites/default/files/filerepository/";
+$file_dir = "http://beta.thorp2000.co.uk/sites/default/files/filerepository/";
 
 //DONT CHANGE ANYTHING BELOW HERE--------------------------------------------------------------------------------------------------------------------
 header('Content-Type: text/html; charset=iso-8859-1');
@@ -242,7 +242,7 @@ $outtext = preg_replace('/(<br\s*\/?>\s*)+/', "<br>", $outtext);
 
 function parseExcel($userDoc)
 {
-require_once '/sites/all/modules/cert-php/excel_reader2.php';
+require_once 'http://beta.thorp2000.co.uk/sites/all/modules/cert-php/excel_reader2.php';
 $data1 = new Spreadsheet_Excel_Reader($userDoc);
 return $data1->dump($row_numbers=false,$col_letters=false);
 }
