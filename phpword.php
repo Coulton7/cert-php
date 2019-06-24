d94744e050b08f2838373de7bca57b0c8e5301dc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-06-24 13:56:27 +0100

    moving the pareseowrd function to see if has an effect on the document

--- phpword.php
+++ phpword.php
@@ -34,6 +34,115 @@ function fileExists($url){
       if( $httpCode == 200 ){return true;}
 }
 
+function parseWord($userDoc)
+{
+    $fileHandle = fopen($userDoc, "r");
+    $word_text = @fread($fileHandle, filesize($userDoc));
+    $line = ""; $lineord = "";
+    $tam = filesize($userDoc);
+    $nulos = 0;
+    $caracteres = 0;
+   // for($i=1536; $i<$tam; $i++)
+    for($i=2536; $i<$tam; $i++)
+    {
+        $line .= $word_text[$i];
+
+		if( ord($word_text[$i]) != 0)
+		$lineord .= ord($word_text[$i]);
+
+        if( $word_text[$i] == 0)
+        {
+            $nulos++;
+        }
+        else
+        {
+            $nulos=0;
+            $caracteres++;
+        }
+
+        if( $nulos>1996 || strstr($lineord,"13131313"))
+        {
+            break;
+        }
+    }
+    //echo $caracteres;
+    $lines = explode(chr(0x0D),$line);
+    //$outtext = "<pre>";
+
+    $outtext = "";
+    foreach($lines as $thisline)
+    {
+        $tam = strlen($thisline);
+        if( !$tam )
+        {
+            continue;
+        }
+
+        $new_line = "";
+        for($i=0; $i<$tam; $i++)
+        {
+            $onechar = $thisline[$i];
+            if( $onechar > chr(240) )
+            {
+                continue;
+            }
+
+            if( $onechar >= chr(0x20) )
+            {
+                $caracteres++;
+                $new_line .= $onechar;
+            }
+
+            if( $onechar == chr(0x14) )
+            {
+                $new_line .= "</a>";
+            }
+
+            if( $onechar == chr(0x07) )
+            {
+                //$new_line .= "\t";
+				$new_line .= "";
+                if( isset($thisline[$i+1]) )
+                {
+                    if( $thisline[$i+1] == chr(0x07) )
+                    {
+                        $new_line .= "\n";
+						//$new_line .= "<br>";
+                    }
+                }
+            }
+        }
+        //troca por hiperlink
+        $new_line = str_replace("HYPERLINK" ,"<a href=",$new_line);
+        $new_line = str_replace("\o" ,">",$new_line);
+        $new_line .= "\n";
+
+        //link de imagens
+        $new_line = str_replace("INCLUDEPICTURE" ,"<br><img src=",$new_line);
+        $new_line = str_replace("\*" ,"><br>",$new_line);
+
+
+		//$new_line = str_replace("&nbsp;" ," ",$new_line);
+        $new_line = str_replace("MERGEFORMATINET" ,"",$new_line);
+		//$new_line=preg_replace('`[\r\n]+`',"\n",$new_line);
+
+		//$new_line = preg_replace('/\n\s+/', "\n", $new_line );
+		//$new_line =preg_replace('#<br>(\s*<br>)+#', '<br>', $new_line);
+		//$new_line = preg_replace('~(^<br>\s*)|((?<=<br>)\s*<br>)|(<br>\s*$)~', '', $new_line);
+		//
+
+		$outtext .= nl2br($new_line);
+
+ // nl2br($new_line);
+
+    }
+$outtext = str_replace("\r","",$outtext);
+$outtext = str_replace("\n","",$outtext);
+
+$outtext = preg_replace('/(<br\s*\/?>\s*)+/', "<br>", $outtext);
+ return $outtext;
+}
+
 
 if (fileExists($userDoc.".doc") || fileExists($userDoc.".xls")) {
 
@@ -117,127 +226,6 @@ to decide where the text is:
 - stitch the rest together again
 - clean up with a regular expression
 *****************************************************************/
-
-
-
-
-
-function parseWord($userDoc)
-{
-    $fileHandle = fopen($userDoc, "r");
-    $word_text = @fread($fileHandle, filesize($userDoc));
-    $line = ""; $lineord = "";
-    $tam = filesize($userDoc);
-    $nulos = 0;
-    $caracteres = 0;
-   // for($i=1536; $i<$tam; $i++)
-    for($i=2536; $i<$tam; $i++)
-    {
-        $line .= $word_text[$i];
-
-		if( ord($word_text[$i]) != 0)
-		$lineord .= ord($word_text[$i]);
-
-        if( $word_text[$i] == 0)
-        {
-            $nulos++;
-        }
-        else
-        {
-            $nulos=0;
-            $caracteres++;
-        }
-
-        if( $nulos>1996 || strstr($lineord,"13131313"))
-        {
-            break;
-        }
-    }
-
-    //echo $caracteres;
-
-    $lines = explode(chr(0x0D),$line);
-    //$outtext = "<pre>";
-
-    $outtext = "";
-    foreach($lines as $thisline)
-    {
-        $tam = strlen($thisline);
-        if( !$tam )
-        {
-            continue;
-        }
-
-        $new_line = "";
-        for($i=0; $i<$tam; $i++)
-        {
-            $onechar = $thisline[$i];
-            if( $onechar > chr(240) )
-            {
-                continue;
-            }
-
-            if( $onechar >= chr(0x20) )
-            {
-                $caracteres++;
-                $new_line .= $onechar;
-            }
-
-            if( $onechar == chr(0x14) )
-            {
-                $new_line .= "</a>";
-            }
-
-            if( $onechar == chr(0x07) )
-            {
-                //$new_line .= "\t";
-				$new_line .= "";
-                if( isset($thisline[$i+1]) )
-                {
-                    if( $thisline[$i+1] == chr(0x07) )
-                    {
-                        $new_line .= "\n";
-						//$new_line .= "<br>";
-                    }
-                }
-            }
-        }
-        //troca por hiperlink
-        $new_line = str_replace("HYPERLINK" ,"<a href=",$new_line);
-        $new_line = str_replace("\o" ,">",$new_line);
-        $new_line .= "\n";
-
-        //link de imagens
-        $new_line = str_replace("INCLUDEPICTURE" ,"<br><img src=",$new_line);
-        $new_line = str_replace("\*" ,"><br>",$new_line);
-
-
-		//$new_line = str_replace("&nbsp;" ," ",$new_line);
-        $new_line = str_replace("MERGEFORMATINET" ,"",$new_line);
-		//$new_line=preg_replace('`[\r\n]+`',"\n",$new_line);
-
-		//$new_line = preg_replace('/\n\s+/', "\n", $new_line );
-		//$new_line =preg_replace('#<br>(\s*<br>)+#', '<br>', $new_line);
-		//$new_line = preg_replace('~(^<br>\s*)|((?<=<br>)\s*<br>)|(<br>\s*$)~', '', $new_line);
-		//
-
-
-		$outtext .= nl2br($new_line);
-
-
-
- // nl2br($new_line);
-
-
-
-    }
-$outtext = str_replace("\r","",$outtext);
-$outtext = str_replace("\n","",$outtext);
-
-$outtext = preg_replace('/(<br\s*\/?>\s*)+/', "<br>", $outtext);
-
- return $outtext;
-}
 
 
 function parseExcel($userDoc)
