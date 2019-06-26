3695f2a01c26bdbba3aa379b03b35bae116c83ec
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-06-26 11:23:52 +0100

    Forgot to update phpword

--- phpword.php
+++ phpword.php
@@ -232,7 +232,7 @@ $outtext = preg_replace('/(<br\s*\/?>\s*)+/', "<br>", $outtext);
 function parseExcel($userDoc)
 {
 require dirname(__FILE__) . '/excel_reader2.php';
-$data1 = new Spreadsheet_Excel_Reader_Constructor($userDoc);
+$data1 = new Spreadsheet_Excel_Reader($userDoc);
 return $data1->dump($row_numbers=false,$col_letters=false);
 }
 
